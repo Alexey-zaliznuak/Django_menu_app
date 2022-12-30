2dc696f1a49165a304caf115f478481c60416aba
Alexey-zaliznuak <zaliznuak20@gmail.com>
2022-12-30 21:15:26 +0300

    use django templates

--- app/menu/templatetags/draw_menu.py
+++ app/menu/templatetags/draw_menu.py
@@ -1,20 +1,12 @@
-from django import template
+from django.template import Library, loader, Template
 from ..models import Menu
 from django.utils.safestring import mark_safe
 
-register = template.Library()
+register = Library()
 
 
 @register.simple_tag(takes_context=True)
 def draw_menu(context, menu_name):
     menu = Menu.objects.filter(name=menu_name)
-
     html = '\n'.join([m.render(context['request']) for m in menu])
-    html = (
-        '<ul style="list-style-type: circle; padding: 0; margin: 10px;"'
-        + html
-        + '</ul>'
-    )
-    html = mark_safe(html)
-
-    return html
+    return loader.render_to_string('draw_menu_container.html', {'html':html})
